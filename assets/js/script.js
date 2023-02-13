e19d9911ae82c0859742621ed4be81de8d56cf18
BobbyCharms <robertcharming@Gmail.com>
2023-02-13 10:09:20 -0500

    first commit + JS pseudocode outline complete

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// When I open the page, I am prompted to start playing a quiz 
+// Once I click start quiz, a timer starts and a question is asked with options 1-4 displayed 
+// If the correct answer is selected, the next question is asked until 5 questions have bene asked, then final time is displayed
+// If incorrect answer is selected, the timer decrements by 10 seconds and the next question is asked, until 5 seconds have been asked, then final time is displayed
+// At the end of the game, the user is prompted to input their initials and their high score is recorded 
+// They are then prompted if they'd like to play again
+// View High Schools is always visible in the upper left hand side
+
+// DEPENENCIES
+
+// FUNCTIONS
+
+// USER INTERACTIONS 
